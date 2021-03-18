810a3f2c57839badd9348d2e70b59de91fe6a73e
L1MeN9Yu <baal998.mophisto@gmail.com>
2021-03-18 22:14:59 +0800

    update UITextField

--- Sources/UIKit/View/Chain.UITextField.swift
+++ Sources/UIKit/View/Chain.UITextField.swift
@@ -14,8 +14,8 @@ public extension ChainBox where T: UITextField {
     }
 
     @discardableResult
-    func font(_ font: UIFont?) -> ChainBox<T> {
-        value.font = font
+    func attributedText(_ attributedText: NSAttributedString?) -> ChainBox<T> {
+        value.attributedText = attributedText
         return value.x
     }
 
@@ -26,8 +26,8 @@ public extension ChainBox where T: UITextField {
     }
 
     @discardableResult
-    func attributedText(_ attributedText: NSAttributedString?) -> ChainBox<T> {
-        value.attributedText = attributedText
+    func font(_ font: UIFont?) -> ChainBox<T> {
+        value.font = font
         return value.x
     }
 
@@ -44,6 +44,12 @@ public extension ChainBox where T: UITextField {
     }
 
     @discardableResult
+    func defaultTextAttributes(_ defaultTextAttributes: [NSAttributedString.Key: Any]) -> ChainBox<T> {
+        value.defaultTextAttributes = defaultTextAttributes
+        return value.x
+    }
+
+    @discardableResult
     func placeholder(_ placeholder: String?) -> ChainBox<T> {
         value.placeholder = placeholder
         return value.x
@@ -56,8 +62,50 @@ public extension ChainBox where T: UITextField {
     }
 
     @discardableResult
+    func clearsOnBeginEditing(_ clearsOnBeginEditing: Bool) -> ChainBox<T> {
+        value.clearsOnBeginEditing = clearsOnBeginEditing
+        return value.x
+    }
+
+    @discardableResult
+    func adjustsFontSizeToFitWidth(_ adjustsFontSizeToFitWidth: Bool) -> ChainBox<T> {
+        value.adjustsFontSizeToFitWidth = adjustsFontSizeToFitWidth
+        return value.x
+    }
+
+    @discardableResult
+    func minimumFontSize(_ minimumFontSize: CGFloat) -> ChainBox<T> {
+        value.minimumFontSize = minimumFontSize
+        return value.x
+    }
+
+    @discardableResult
     func delegate(_ delegate: UITextFieldDelegate?) -> ChainBox<T> {
         value.delegate = delegate
+        return value.x
+    }
+
+    @discardableResult
+    func background(_ background: UIImage?) -> ChainBox<T> {
+        value.background = background
+        return value.x
+    }
+
+    @discardableResult
+    func disabledBackground(_ disabledBackground: UIImage?) -> ChainBox<T> {
+        value.disabledBackground = disabledBackground
+        return value.x
+    }
+
+    @discardableResult
+    func allowsEditingTextAttributes(_ allowsEditingTextAttributes: Bool) -> ChainBox<T> {
+        value.allowsEditingTextAttributes = allowsEditingTextAttributes
+        return value.x
+    }
+
+    @discardableResult
+    func typingAttributes(_ typingAttributes: [NSAttributedString.Key: Any]?) -> ChainBox<T> {
+        value.typingAttributes = typingAttributes
         return value.x
     }
 
@@ -68,25 +116,51 @@ public extension ChainBox where T: UITextField {
     }
 
     @discardableResult
-    func leftView(_ leftView: UIView?, mode: UITextField.ViewMode) -> ChainBox<T> {
+    func leftView(_ leftView: UIView?) -> ChainBox<T> {
         value.leftView = leftView
-        value.leftViewMode = mode
         return value.x
     }
 
     @discardableResult
-    func rightView(_ rightView: UIView?, mode: UITextField.ViewMode) -> ChainBox<T> {
+    func leftViewMode(_ leftViewMode: UITextField.ViewMode) -> ChainBox<T> {
+        value.rightViewMode = leftViewMode
+        return value.x
+    }
+
+    @discardableResult
+    func rightView(_ rightView: UIView?) -> ChainBox<T> {
         value.rightView = rightView
-        value.rightViewMode = mode
         return value.x
     }
 
     @discardableResult
-    func isSecureTextEntry(_ isSecureTextEntry: Bool = true) -> ChainBox<T> {
-        value.isSecureTextEntry = isSecureTextEntry
+    func rightViewMode(_ rightViewMode: UITextField.ViewMode) -> ChainBox<T> {
+        value.rightViewMode = rightViewMode
         return value.x
     }
 
+    @discardableResult
+    func inputView(_ inputView: UIView?) -> ChainBox<T> {
+        value.inputView = inputView
+        return value.x
+    }
+
+    @discardableResult
+    func inputAccessoryView(_ inputAccessoryView: UIView?) -> ChainBox<T> {
+        value.inputAccessoryView = inputAccessoryView
+        return value.x
+    }
+
+    @discardableResult
+    func clearsOnInsertion(_ clearsOnInsertion: Bool) -> ChainBox<T> {
+        value.clearsOnInsertion = clearsOnInsertion
+        return value.x
+    }
+}
+
+// MARK: - UIInputTraits
+
+public extension ChainBox where T: UITextField {
     @discardableResult
     func autocapitalizationType(_ autocapitalizationType: UITextAutocapitalizationType) -> ChainBox<T> {
         value.autocapitalizationType = autocapitalizationType
@@ -96,6 +170,72 @@ public extension ChainBox where T: UITextField {
     @discardableResult
     func autocorrectionType(_ autocorrectionType: UITextAutocorrectionType) -> ChainBox<T> {
         value.autocorrectionType = autocorrectionType
+        return value.x
+    }
+
+    @discardableResult
+    func spellCheckingType(_ spellCheckingType: UITextSpellCheckingType) -> ChainBox<T> {
+        value.spellCheckingType = spellCheckingType
+        return value.x
+    }
+
+    @discardableResult
+    func smartQuotesType(_ smartQuotesType: UITextSmartQuotesType) -> ChainBox<T> {
+        value.smartQuotesType = smartQuotesType
+        return value.x
+    }
+
+    @discardableResult
+    func smartDashesType(_ smartDashesType: UITextSmartDashesType) -> ChainBox<T> {
+        value.smartDashesType = smartDashesType
+        return value.x
+    }
+
+    @discardableResult
+    func smartInsertDeleteType(_ smartInsertDeleteType: UITextSmartInsertDeleteType) -> ChainBox<T> {
+        value.smartInsertDeleteType = smartInsertDeleteType
+        return value.x
+    }
+
+    @discardableResult
+    func keyboardType(_ keyboardType: UIKeyboardType) -> ChainBox<T> {
+        value.keyboardType = keyboardType
+        return value.x
+    }
+
+    @discardableResult
+    func keyboardAppearance(_ keyboardAppearance: UIKeyboardAppearance) -> ChainBox<T> {
+        value.keyboardAppearance = keyboardAppearance
+        return value.x
+    }
+
+    @discardableResult
+    func returnKeyType(_ returnKeyType: UIReturnKeyType) -> ChainBox<T> {
+        value.returnKeyType = returnKeyType
+        return value.x
+    }
+
+    @discardableResult
+    func enablesReturnKeyAutomatically(_ enablesReturnKeyAutomatically: Bool) -> ChainBox<T> {
+        value.enablesReturnKeyAutomatically = enablesReturnKeyAutomatically
+        return value.x
+    }
+
+    @discardableResult
+    func isSecureTextEntry(_ isSecureTextEntry: Bool) -> ChainBox<T> {
+        value.isSecureTextEntry = isSecureTextEntry
+        return value.x
+    }
+
+    @discardableResult
+    func textContentType(_ textContentType: UITextContentType?) -> ChainBox<T> {
+        value.textContentType = textContentType
+        return value.x
+    }
+
+    @discardableResult
+    func passwordRules(_ passwordRules: UITextInputPasswordRules?) -> ChainBox<T> {
+        value.passwordRules = passwordRules
         return value.x
     }
 }
